0d9c680e1b26362844bc274d1cc91d2a7efba9cb
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-12-24 00:37:50 -0500

    refactor: moved `notator` to a standalone plugin `c-dilks/notator.nvim`

--- .config/nvim/lua/plugins/notator.lua
+++ .config/nvim/lua/plugins/notator.lua
@@ -1,14 +1,13 @@
 return {
-  dir = vim.fn.stdpath('config') .. '/lua/notator',
+  'c-dilks/notator.nvim',
   lazy = false,
   opts = {
-    notation_table = {
+    tag_table = {
       { name = 'PRIORITY', color = 'yellow'     },
       { name = 'TODO',     color = 'red'        },
       { name = 'RUNNING',  color = 'cyan'       },
       { name = 'WAITING',  color = 'magenta'    },
       { name = 'DONE',     color = 'lightgreen' },
     },
-    fixed_width = false,
-  }
+  },
 }

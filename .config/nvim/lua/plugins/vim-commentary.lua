b6caedd0f7b7a68fec335962e121fcc73dd272cd
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-12-25 01:55:07 -0500

    modified:   .config/nvim/lua/plugins/vim-commentary.lua

--- .config/nvim/lua/plugins/vim-commentary.lua
+++ .config/nvim/lua/plugins/vim-commentary.lua
@@ -1,6 +1,7 @@
 return {
   'tpope/vim-commentary',
   keys = {{ [[gc]], mode={'n','x'}}},
+  lazy = false,
   config = function()
     vim.cmd([[au FileType cpp setlocal commentstring=//%s]])
   end,

a02fd945b398ab690639caf27f8a180140d96ed5
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-01-05 12:56:27 -0500

    fix: disable tree-sitter syntax coloring for now, since unstable

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,10 +6,11 @@ return {
     config = function()
       require('nvim-treesitter.install').compilers = {'gcc'} -- since ifarm `cc` is ancient
       require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
         sync_install = false,
         auto_install = true,
         highlight = {
-          enable = true,
+          enable = false, -- too many issues at the moment...
           additional_vim_regex_highlighting = true,
           disable = {
             'markdown', -- since spellchecking is too agressive when tree-sitter is used

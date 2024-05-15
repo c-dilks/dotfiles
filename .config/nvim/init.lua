a09675447c1ae3caf8d14e4cafb9775f0aaee9ba
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-05-14 21:24:39 -0400

    feat: nvim transparent

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,9 @@ require('config.settings')
 require('config.filetypes')
 require('config.keys')
 require('config.lazy')
+
+-- transparent bg
+vim.cmd('highlight Normal guibg=none')
+vim.cmd('highlight NonText guibg=none')
+vim.cmd('highlight Normal ctermbg=none')
+vim.cmd('highlight NonText ctermbg=none')

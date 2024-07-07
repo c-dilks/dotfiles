57f27c7b8ce3b06ab48e4d021445fe7240453d53
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-07-07 13:54:44 -0400

    modified:   functions/fish_prompt.fish

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
-function fish_prompt --description 'Informative prompt'
+function fish_prompt --description 'Adapted from Informative prompt'
     #Save the return status of the previous command
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
@@ -13,8 +13,21 @@ function fish_prompt --description 'Informative prompt'
         set -l statusb_color (set_color --bold $fish_color_status)
         set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-        printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
-            $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
-            (set_color normal)
+        # set -g __fish_git_prompt_show_informative_status 1 # too slow for big repos
+        set -g __fish_git_prompt_showdirtystate true
+        set -g __fish_git_prompt_showupstream auto
+        set -g __fish_git_prompt_color_branch brmagenta
+        set -g __fish_git_prompt_color_stagedstate bryellow
+        set -g __fish_git_prompt_color_invalidstate brred
+        set -g __fish_git_prompt_color_cleanstate brgreen
+
+        printf '%s<<<<<<<<<<< %s%s%s\n %s:%s   %s[%s] %s%s@%s%s%s\n%s %s>>>%s ' \
+          (set_color -b 333333 brgreen) \
+          (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) \
+          (set_color -b 333333 brgreen) (set_color normal) \
+          (set_color brmagenta) (date '+%a %T') \
+          (set_color brgreen) $USER (prompt_hostname) \
+          (set_color normal) (fish_git_prompt) \
+          $pipestatus_string (set_color brgreen)
     end
 end

2249f905bb4d17790abe0fc229c0a5e7e84bbde9
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-09-18 12:38:42 -0400

    feat: abbr `//`

--- .config/fish/conf.d/bang_bang.fish
+++ .config/fish/conf.d/bang_bang.fish
@@ -1,4 +1,8 @@
 function bang_bang
   echo $history[1]
 end
+function slash_slash
+  echo $history[1]/
+end
 abbr -a !! --position anywhere --function bang_bang
+abbr -a // --position anywhere --function slash_slash

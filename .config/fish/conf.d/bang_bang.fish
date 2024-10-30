4a50f3d6d69b9033c86e42bf92f4bb9027d2ab13
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-10-30 15:22:28 -0400

    fix: use `plugin-bang-bang` instead of `abbr`

--- .config/fish/conf.d/bang_bang.fish
+++ .config/fish/conf.d/bang_bang.fish
@@ -1,8 +1,67 @@
-function bang_bang
-  echo $history[1]
+########################################################################################
+# NOTE: these functions were copied from https://github.com/oh-my-fish/plugin-bang-bang;
+#       the copyright and license are included below
+########################################################################################
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Aerys Bat
+# Copyright (c) 2015 Derek Stavis
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
 end
-function slash_slash
-  echo $history[1]/
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
 end
-abbr -a !! --position anywhere --function bang_bang
-abbr -a // --position anywhere --function slash_slash
+
+function _plugin-bang-bang_key_bindings --on-variable fish_key_bindings
+    bind --erase !
+    bind --erase '$'
+    switch "$fish_key_bindings"
+    case 'fish_default_key_bindings'
+        bind --mode default ! __history_previous_command
+        bind --mode default '$' __history_previous_command_arguments
+    case 'fish_vi_key_bindings' 'fish_hybrid_key_bindings'
+        bind --mode insert ! __history_previous_command
+        bind --mode insert '$' __history_previous_command_arguments
+    end
+end
+
+function _plugin-bang-bang_uninstall --on-event plugin-bang-bang_uninstall
+    bind --erase !
+    bind --erase '$'
+    functions --erase _plugin-bang-bang_uninstall
+end
+
+_plugin-bang-bang_key_bindings

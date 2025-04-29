f41219efd74961273749127d49f0478dc6be75c5
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-04-29 13:03:14 -0400

    fix(diffview.nvim): disable keybinding for `X`, which is too easy to accidentally press and too difficult to undo the action

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -6,5 +6,15 @@ return {
   {
     'sindrets/diffview.nvim',
     lazy = false,
+    config = function()
+      local P = require('diffview')
+      P.setup({
+        keymaps = {
+          file_panel = {
+            { "n", "X", false, { desc = "Restore entry to the state on the left side" } }, -- disabled, since easy to accidentally do, and difficult to deliberately undo
+          },
+        },
+      })
+    end
   },
 }

1432c60ca33cad15dd956eaeca2f03a1fce01b05
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-09-02 18:42:04 -0400

    fix: allow `...` diffview to be modifiable

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -14,6 +14,7 @@ return {
             { "n", "X", false, { desc = "Restore entry to the state on the left side" } }, -- disabled, since easy to accidentally do, and difficult to deliberately undo
           },
         },
+        default_args = { DiffviewOpen = { "--imply-local" }, }, -- allow ':DiffviewOpen origin/main...HEAD' to be modifiable (see https://github.com/sindrets/diffview.nvim/issues/527)
       })
     end
   },
